9ccf4b1c5648b480de7efc36fbcbdcb8ddddf05a
TejasAnanda <cs16btech11043@iith.ac.in>
2018-03-01 21:54:42 +0530

    add readMouse for relative pos and events in host.g (updated with rx and ry)o

--- hostd/host.go
+++ hostd/host.go
@@ -18,17 +18,21 @@ func check(e error) {
 }
 
 func readMouse(f *os.File) {
-	var l,r,mid int 
+	var l,r,mid,xr,yr int 
 	b1 := make([]byte, 24)
 	f.Read(b1)
 
-	fmt.Println(b1)
+	//fmt.Println(b1)
 
 	l=int(b1[0] & 0x1)
 	r=int(b1[0] & 0x2)
 	mid=int(b1[0] & 0x4)
+    xr=int(b1[1])
+    yr=int(b1[2])
 
-	fmt.Printf("left=%d , right=%d , middle=%d \n",l,r/2,mid/4)
+	fmt.Printf("left=%d , right=%d , middle=%d \n",r/2,l,mid/4)
+	fmt.Printf("xr=%d , yr=%d \n",xr,yr)
+    
 }
 
 func main() {
