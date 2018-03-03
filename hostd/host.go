b1758c0b2c4bb2e0dcea347424b5471aaffcf433
CrazyByDefault <es16btech11025@iith.ac.in>
2018-03-03 16:40:37 +0530

    Added keylogger files

--- hostd/host.go
+++ hostd/host.go
@@ -1,50 +1,46 @@
-// Tejas, add your code here
-
 package main
 
 import (
-    "fmt"
-   // "io"
-    //"io/ioutil"
-    "os"
+	"fmt"
+	// "io"
+	//"io/ioutil"
+	"os"
 )
 
 // Reading files requires checking most calls for errors.
 // This helper will streamline our error checks below.
 func check(e error) {
-    if e != nil {
-        panic(e)
-    }
+	if e != nil {
+		panic(e)
+	}
 }
 
 func readMouse(f *os.File) {
-	var l,r,mid int 
+	var l, r, mid int
 	b1 := make([]byte, 24)
 	f.Read(b1)
 
 	fmt.Println(b1)
 
-	l=int(b1[0] & 0x1)
-	r=int(b1[0] & 0x2)
-	mid=int(b1[0] & 0x4)
+	l = int(b1[0] & 0x1)
+	r = int(b1[0] & 0x2)
+	mid = int(b1[0] & 0x4)
 
-	fmt.Printf("left=%d , right=%d , middle=%d \n",l,r/2,mid/4)
+	fmt.Printf("left=%d , right=%d , middle=%d \n", l, r/2, mid/4)
 }
 
 func main() {
 
-	device := "/dev/input/mouse0"    
+	device := "/dev/input/mouse0"
 
-    // You'll often want more control over how and what
-    // parts of a file are read. For these tasks, start
-    // by `Open`ing a file to obtain an `os.File` value.
-    f, err := os.Open(device)
-    check(err)
+	// You'll often want more control over how and what
+	// parts of a file are read. For these tasks, start
+	// by `Open`ing a file to obtain an `os.File` value.
+	f, err := os.Open(device)
+	check(err)
 
-
-    for true {
-		readMouse(f)        
-    }
-     
+	for true {
+		readMouse(f)
+	}
 
 }
--- main.go
+++ main.go
@@ -1,7 +1,36 @@
 package main
 
-import "./hostd/host"
+import (
+	"fmt"
+
+	"./keylogger"
+)
 
 func main() {
-	host.main()
+	devs, err := keylogger.NewDevices()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	for _, val := range devs {
+		fmt.Println("Id->", val.Id, "Device->", val.Name)
+	}
+
+	//keyboard device file, on your system it will be diffrent!
+	rd := keylogger.NewKeyLogger(devs[2])
+
+	in, err := rd.Read()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	for i := range in {
+
+		//we only need keypress
+		if i.Type == keylogger.EV_KEY {
+			fmt.Println(i.KeyString())
+		}
+	}
 }
