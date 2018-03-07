d846d90285712be5db3c43ff3d51140f7e93b69e
CrazyByDefault <es16btech11025@iith.ac.in>
2018-03-07 09:39:49 +0530

    fix merge conflicts (forgot to press ctrl+s, I need sleep)

--- clientd/mouselogger/rel.go
+++ clientd/mouselogger/rel.go
@@ -1,21 +1,9 @@
-<<<<<<< Updated upstream
-package mouselogger
-=======
 package mousemover
->>>>>>> Stashed changes
 
 import (
 	//"fmt"
 	// "io"
 	//"io/ioutil"
-<<<<<<< Updated upstream
-	"os"
-)
-
-type Activity struct {
-	Rx, Ry, Ri, Le, Mid int
-}
-=======
 
 	"os/exec"
 
@@ -25,7 +13,6 @@ type Activity struct {
 // type Activity struct {
 // 	Rx, Ry, Ri, Le, Mid int
 // }
->>>>>>> Stashed changes
 
 // Reading files requires checking most calls for errors.
 // This helper will streamline our error checks below.
@@ -35,23 +22,6 @@ func check(e error) {
 	}
 }
 
-<<<<<<< Updated upstream
-func readMouse(f *os.File, ch chan Activity) {
-	//var l, r, mid, xr, yr int
-	b1 := make([]byte, 24)
-	f.Read(b1)
-
-	//fmt.Println(b1)
-	a := Activity{}
-	a.Ri = int(b1[0] & 0x1)
-	a.Le = int(b1[0]&0x2) / 2
-	a.Mid = int(b1[0]&0x4) / 4
-	a.Rx = int(b1[1])
-	a.Ry = int(b1[2])
-
-	ch <- a
-
-=======
 func readMouse(ch chan mouselogger.Activity) {
 	//var l, r, mid, xr, yr int
 	var x, y string
@@ -83,31 +53,7 @@ func readMouse(ch chan mouselogger.Activity) {
 	} else {
 		exec.Command("xdotool", "mousemove_relative", "--", x, y).Run()
 	}
->>>>>>> Stashed changes
 	// fmt.Printf("left=%d , right=%d , middle=%d \n", r/2, l, mid/4)
 	// fmt.Printf("xr=%d , yr=%d \n", xr, yr)
 
 }
-<<<<<<< Updated upstream
-
-func GetMouseRel(ch chan Activity) {
-
-	device := "/dev/input/mouse0"
-
-	// You'll often want more control over how and what
-	// parts of a file are read. For these tasks, start
-	// by `Open`ing a file to obtain an `os.File` value.
-	f, err := os.Open(device)
-	check(err)
-
-	// You'll often want more control over how and what
-	// parts of a file are read. For these tasks, start
-	// by `Open`ing a file to obtain an `os.File` value.
-
-	//	for true {
-	readMouse(f, ch)
-	//	}
-
-}
-=======
->>>>>>> Stashed changes
