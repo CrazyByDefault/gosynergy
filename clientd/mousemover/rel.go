b43b85015ec5dfe3c4502da2294f37a53904e23e
TejasAnanda <cs16btech11043@iith.ac.in>
2018-03-07 11:52:01 +0530

    udpates movemover for be more fluid

--- clientd/mousemover/rel.go
+++ clientd/mousemover/rel.go
@@ -19,6 +19,13 @@ func check(e error) {
 		panic(e)
 	}
 }
+func max(a, b int) int {
+	if a >= b {
+		return a
+	} else {
+		return b
+	}
+}
 
 // ReadMouse reads and executes the mouse events in the 'current' Activity struct
 func ReadMouse(current Activity, leftState int, rightState int, midState int) (int, int, int) {
@@ -35,56 +42,63 @@ func ReadMouse(current Activity, leftState int, rightState int, midState int) (i
 	// a.Ry = int(b1[2])
 
 	if current.Rx > 0 && current.Rx < 50 {
-		x = "10"
+		x = "1"
 	} else if current.Rx > 0 && current.Rx > 200 {
-		x = "-10"
+		x = "-1"
+		current.Rx = 256 - current.Rx
 	} else {
 		x = "0"
 	}
 	if current.Ry > 0 && current.Ry < 50 {
-		y = "-10"
+		y = "-1"
 	} else if current.Ry > 0 && current.Ry > 200 {
-		y = "10"
+		y = "1"
+		current.Ry = 256 - current.Ry
 	} else {
 		y = "0"
 	}
 
 	if current.Ri != leftState {
 		var event string
+		leftState = current.Ri
 		if leftState == 0 {
 			event = "mouseup"
 		} else {
 			event = "mousedown"
 		}
-		leftState = current.Ri
+
 		exec.Command("xdotool", event, "1").Run()
 		fmt.Println(event + "1")
 
 	} else if current.Le != rightState {
 		var event string
+		rightState = current.Le
 		if current.Le == 0 {
 			event = "mouseup"
 		} else {
 			event = "mousedown"
 		}
-		rightState = current.Le
+
 		exec.Command("xdotool", event, "3").Run()
 		fmt.Println(event + "2")
 
 	} else if current.Mid != midState {
 		var event string
+		midState = current.Mid
 		if current.Mid == 0 {
 			event = "mouseup"
 		} else {
 			event = "mousedown"
 		}
-		midState = current.Mid
+
 		exec.Command("xdotool", event, "2").Run()
 		fmt.Println(event + "3")
 	}
+	m := max(current.Rx, current.Ry)
 
-	exec.Command("xdotool", "mousemove_relative", "--", x, y).Run()
-	// fmt.Printf("left=%d , right=%d , middle=%d \n", r/2, l, mid/4)
-	// fmt.Printf("xr=%d , yr=%d \n", xr, yr)
+	for i := 0; i < m*10; i++ {
+
+		exec.Command("xdotool", "mousemove_relative", "--", x, y).Run()
+	}
 	return leftState, rightState, midState
 }
