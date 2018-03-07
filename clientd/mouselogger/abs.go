6192fbac20feaadf82750d9deb73681806e4c3a5
CrazyByDefault <es16btech11025@iith.ac.in>
2018-03-07 09:38:18 +0530

    get movemouse funcs from master

--- clientd/mouselogger/abs.go
+++ clientd/mouselogger/abs.go
@@ -1,33 +1,35 @@
-package mouselogger
+package mousemover
 
 import (
 	//"fmt"
 	"os/exec"
 	//"time"
 	"strconv"
-	"strings"
 )
 
-type Cords struct {
-	X, Y int
-}
+// type Cords struct {
+// 	X, Y int
+// }
 
-func GetMouseAbs(ch chan Cords) {
+func GetMouseAbs() {
 	// Create an *exec.Cmd
 	//str:="xdpyinfo  | grep -oP 'dimensions:\s+\K\S+'"
 	//cmd1,_ := exec.Command("bash" , "-c", str).Output()
 	//fmt.Println(cmd1)
 
 	//for true {
-	cmd, _ := exec.Command("xdotool", "getmouselocation").Output()
+	x := strconv.Itoa(39)
+	y := strconv.Itoa(39)
 
-	s := strings.Split(strings.Replace(string(cmd), ":", " ", -1), " ")
-	c := Cords{}
-	x, _ := strconv.Atoi(s[1])
-	y, _ := strconv.Atoi(s[3])
-	//wid,_:= strconv.Atoi(s[7])
-	c.X = x
-	c.Y = y
+	exec.Command("xdotool", "mousemove", x, y).Run()
+
+	// s := strings.Split(strings.Replace(string(cmd), ":", " ", -1), " ")
+	// c := Cords{}
+	// x, _ := strconv.Atoi(s[1])
+	// y, _ := strconv.Atoi(s[3])
+	// //wid,_:= strconv.Atoi(s[7])
+	// c.X = x
+	// c.Y = y
 
 	//strings.Replace(s, ":", " ", -1)
 	//s:=strings.Split(s, " ")
@@ -38,7 +40,7 @@ func GetMouseAbs(ch chan Cords) {
 	// cmd.Stdout = cmdOutput
 
 	// Execute command
-	ch <- c
+	// ch <- c
 	//fmt.Println(x,y)
 	//fmt.Println(wid)
 	//fmt.Println(s[7])
--- clientd/mouselogger/rel.go
+++ clientd/mouselogger/rel.go
@@ -1,15 +1,31 @@
+<<<<<<< Updated upstream
 package mouselogger
+=======
+package mousemover
+>>>>>>> Stashed changes
 
 import (
 	//"fmt"
 	// "io"
 	//"io/ioutil"
+<<<<<<< Updated upstream
 	"os"
 )
 
 type Activity struct {
 	Rx, Ry, Ri, Le, Mid int
 }
+=======
+
+	"os/exec"
+
+	"../../hostd/mouselogger"
+)
+
+// type Activity struct {
+// 	Rx, Ry, Ri, Le, Mid int
+// }
+>>>>>>> Stashed changes
 
 // Reading files requires checking most calls for errors.
 // This helper will streamline our error checks below.
@@ -19,6 +35,7 @@ func check(e error) {
 	}
 }
 
+<<<<<<< Updated upstream
 func readMouse(f *os.File, ch chan Activity) {
 	//var l, r, mid, xr, yr int
 	b1 := make([]byte, 24)
@@ -34,10 +51,44 @@ func readMouse(f *os.File, ch chan Activity) {
 
 	ch <- a
 
+=======
+func readMouse(ch chan mouselogger.Activity) {
+	//var l, r, mid, xr, yr int
+	var x, y string
+	// ch.Ri = int(b1[0] & 0x1)
+	// ch.Le = int(b1[0]&0x2) / 2
+	// ch.Mid = int(b1[0]&0x4) / 4
+	// a.Rx = int(b1[1])
+	// a.Ry = int(b1[2])
+
+	if ch.Rx < 128 {
+		x = "1"
+	} else {
+		x = "-1"
+	}
+	if ch.Ry < 128 {
+		y = "-1"
+	} else {
+		y = "1"
+	}
+	if ch.Ri == 1 {
+		exec.Command("xdotool", "mousemove_relative", "--", x, y, "click", "3").Run()
+
+	} else if ch.Le == 1 {
+		exec.Command("xdotool", "mousemove_relative", "--", x, y, "click", "1").Run()
+
+	} else if ch.Mid == 1 {
+		exec.Command("xdotool", "mousemove_relative", "--", x, y, "click", "2").Run()
+
+	} else {
+		exec.Command("xdotool", "mousemove_relative", "--", x, y).Run()
+	}
+>>>>>>> Stashed changes
 	// fmt.Printf("left=%d , right=%d , middle=%d \n", r/2, l, mid/4)
 	// fmt.Printf("xr=%d , yr=%d \n", xr, yr)
 
 }
+<<<<<<< Updated upstream
 
 func GetMouseRel(ch chan Activity) {
 
@@ -58,3 +109,5 @@ func GetMouseRel(ch chan Activity) {
 	//	}
 
 }
+=======
+>>>>>>> Stashed changes
